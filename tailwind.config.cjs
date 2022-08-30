0349ec654e82f98af9433e0d1bf14c3ece5d498c
suyash-bureau <pradhansuyash39@gmail.com>
2022-08-30 23:32:04 +0530

    chore: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue: '#1fb6ff',
+				purple: '#7e5bef',
+				pink: '#ff49db',
+				orange: '#ff7849',
+				green: '#13ce66',
+				yellow: '#ffc82c',
+				'gray-dark': '#273444',
+				gray: '#8492a6',
+				'gray-light': '#d3dce6',
+			},
+		},
 	},
 	plugins: [],
 };
